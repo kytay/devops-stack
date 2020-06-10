d3b2ff12eb17aca91ebe6268f08a600000b68634
TKY <->
2020-06-10 23:22:44 +0800

    Commit update on setup.js

--- .devops/bin/setup.js
+++ .devops/bin/setup.js
@@ -3,7 +3,15 @@ const YAML = require('js-yaml');
 const FS = require('fs');
 
 try {
+    // Read all templates from "../tpl" directory
+    let listOfTemplates = FS.readdirSync("../tpl");
+
+    listOfTemplates.forEach(file => {
+        console.log(file);
+    })
+
     const DOC = YAML.safeLoad(FS.readFileSync('../conf/project.yaml', 'utf8'));
+
     console.log(DOC);
 } catch (ex) {
     console.log(ex);
